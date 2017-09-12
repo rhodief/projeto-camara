1b96dc15296f4589697285362a74b906a15aab39
Rhodie <rhandref@gmail.com>
2017-09-12 00:43:43 -0300

    Tornando o player de video do camara ao vivo responsivo

--- public/js/components/panels/panel-tv_camara/panel-tv_camara.js
+++ public/js/components/panels/panel-tv_camara/panel-tv_camara.js
@@ -24,14 +24,16 @@
             var params;
             var parse;
             var url = '';
+            var text = '';
             if(attrs.params) params = attrs.params.split("'").join('"');
             if(params){
                 parse = JSON.parse(params);
                 if(parse && parse.url){
                     url = parse.url;
+                    text = parse.text;
                 }
             }
-            var template = '<div class="panel"><iframe width="300" height="300" ng-src="' + url + '" frameborder="0" allowfullscreen>{{url}}</iframe></div>';
+            var template = '<div class="videowrapper"><iframe width="300" height="300" ng-src="' + url + '" frameborder="0" allowfullscreen>{{url}}</iframe></div><span class="box-text"><span class="title">' + text + '</span><span class="date-category"><span>TV Câmara</span></span></span>';
             var newElement = $compile(template)(scope);
             element.append(newElement);
         }
