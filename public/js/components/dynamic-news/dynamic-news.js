0a7b8e388552804e6736523110d85097cb64239c
Rhodie <rhandref@gmail.com>
2017-10-12 09:42:19 -0300

    Corrigido exibição das notícias personalizadas em Câmara notícias

--- public/js/components/dynamic-news/dynamic-news.js
+++ public/js/components/dynamic-news/dynamic-news.js
@@ -8,15 +8,15 @@
     'use strict';
 
     angular
-        .module ('dynamicNews')
-        .directive ('dynamicNew', dynamicNew);
+        .module('dynamicNews')
+        .directive('dynamicNew', dynamicNew);
 
     dynamicNew.$inject = ['$http', '$rootScope'];
 
     function dynamicNew($http, $rootScope) {
         var directive = {
             link: link,
-            scope:{},
+            scope: {},
             restrict: 'A'
         };
         return directive;
@@ -26,79 +26,97 @@
             var thisOption = attrs.option;
             var thisType = attrs.type;
             var thisElement = element.children();
-            var els, img, box, title, text, dataCat, oldUrl, oldTitle;
+            var els, img, box, hat, title, text, dataCat, oldUrl, oldTitle;
             var replacedNew;
             var oldFav = {};
-            
 
-           scope.$on('activeDynamicNews', function(ev, data){
-               var category = data.category;
-               var option = data.option;
-               if(option === thisOption){
-                   if(category === false) return restore();
-                   return active(category);
-               }
+
+            scope.$on('activeDynamicNews', function (ev, data) {
+                var category = data.category;
+                var option = data.option;
+                if (option === thisOption) {
+                    if (category === false) return restore();
+                    return active(category);
+                }
 
             });
 
-            function active(category){
+            function active(category) {
                 replacedNew = angular.copy(thisElement);
                 els = angular.copy(thisElement);
                 img = angular.element(els[0]).children()[0];
                 box = angular.element(els[1]).children();
-                title = angular.element(box[0]);
-                text = angular.element(box[1]);
-                dataCat = angular.element(box[2]);
+                for (var i = 0; i < box.length; i++) {
+                    var className = box[i].className;
+                    var angElement = angular.element(box[i]);
+                    switch (className) {
+                        case 'hat-text':
+                            hat = angElement;
+                            break;
+                        case 'title':
+                            title = angElement;
+                            break;
+                        case 'text':
+                            text = angElement;
+                            break;
+                        case 'date-category':
+                            dataCat = angElement;
+                            break;
+                    }
+                }
                 oldUrl = oldUrl || angular.copy(attrs.href);
                 _LoadingAnimation(element);
                 _getCategoryNews(category, thisType).then(okNews).catch(error);
             }
 
-            function restore(){
-                if(replacedNew){
+            function restore() {
+                if (replacedNew) {
                     element.html(replacedNew);
                     $rootScope.$broadcast('refrashFavorites', oldFav);
                 }
             }
 
-            function okNews(news){
-                img.src = news.img;
-                img.alt = news.alt;
-                title.text(news.title);
-                text.text(news.text);
-                dataCat.html('<span>' + news.category + '</span><span>' + news.date + '</span>');
+            function okNews(news) {
+                if(img){
+                    img.src = news.img;
+                    img.alt = news.alt;
+                }
+                if(hat) news.hat ? hat.html('<span>' + news.hat + '</span>'): hat.text('-');
+                if(title) title.text(news.title);
+                if(text) text.text(news.text);
+                if(dataCat) dataCat.html('<span>' + news.category + '</span><span>' + news.date + '</span>');
                 element.html(els);
                 attrs.$set('href', news.url);
                 var favorite = element.parent().find('favorites-button');
                 oldTitle = angular.copy(favorite.attr('title'));
                 favorite.attr('url', news.url);
                 favorite.attr('title', news.title);
-                oldFav = {url:{old:news.url, new:oldUrl}, title:oldTitle} //Armazenar para o rollback
-                $rootScope.$broadcast('refrashFavorites', {url:{old:oldUrl, new:news.url}, title:news.title});
+                oldFav = { url: { old: news.url, new: oldUrl }, title: oldTitle } //Armazenar para o rollback
+                $rootScope.$broadcast('refrashFavorites', { url: { old: oldUrl, new: news.url }, title: news.title });
             }
 
         }
 
-        function _getCategoryNews(category, type){
+        function _getCategoryNews(category, type) {
             return $http.get('../server_simulator/dynamic-news/news.json').then(okData).catch(error);
-            function okData(data){
+            function okData(data) {
                 var ret = data.data.dados;
                 var newCategory = ret[category];
-                if(type) newCategory = newCategory['opt' + type];
+                if (type) newCategory = newCategory['opt' + type];
                 return newCategory;
             }
-            
+
         }
 
-        function error(e){
+        function error(e) {
             console.log(e);
         }
 
-        function _LoadingAnimation(element){
+        function _LoadingAnimation(element) {
             element.html(`<span class="loading">Carregando</span>`)
         }
 
-        
+
     }
 
 })();
